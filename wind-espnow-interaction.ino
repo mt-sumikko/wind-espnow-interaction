bf74a598846148a82064dd70d729c72c52abd661
mt-sumikko <mt.sumikko163@gmail.com>
2024-06-01 00:41:24 +0900

    一旦マルチタスクにしておいてみた、windControlはloop内で良さそうだけど

--- wind-espnow-interaction.ino
+++ wind-espnow-interaction.ino
@@ -1,16 +1,16 @@
-//omom m5atom curtain wind relay control
+#include "M5Atom.h"
 #include <esp_now.h>
 #include <WiFi.h>
 
-#include <Arduino.h>
-#include <FastLED.h>  //LED
-
 //LED -----------
-#define PIN_LED 21  // 本体フルカラーLEDの使用端子（G21）S3
-#define NUM_LEDS 1  // 本体フルカラーLEDの数
+uint8_t brightness;
+uint8_t change = 1;//起動時Auto, 1回ボタン押したらInteraction Modeになるようにしておく
 
-CRGB leds[NUM_LEDS];
-//------------------ -
+CRGB dispColor(uint8_t g, uint8_t r, uint8_t b) {
+  return (CRGB)((g << 16) | (r << 8) | b);
+}
+
+//------------------
 
 
 #define FAN_PIN_1 22
@@ -46,9 +46,105 @@ void onReceive(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
   }
 }
 
+
+
+//LED点灯タスクを渡す変数
+//int ledTask = 0;
+int runningMode = 0;//0:Auto, 1:Interaction, 2:Off
+
+//LEDの点灯をコントロール
+void windControl(void* arg) {
+  while (1) {
+    if (runningMode == 0) { //Auto
+      //ledTask = 0;
+
+
+      //非受信時のテスト用 START-----
+      digitalWrite(FAN_PIN_1, HIGH);
+      delay(600);  // 1秒間ファンをを回す
+
+      digitalWrite(FAN_PIN_2, HIGH);
+      delay(600);  // 1秒間ファンをを回す
+      digitalWrite(FAN_PIN_1, LOW);
+
+      digitalWrite(FAN_PIN_3, HIGH);
+      delay(600);  // 1秒間ファンをを回す
+      digitalWrite(FAN_PIN_2, LOW);
+
+      digitalWrite(FAN_PIN_4, HIGH);
+      delay(600);  // 1秒間ファンをを回す
+      digitalWrite(FAN_PIN_3, LOW);
+
+      digitalWrite(FAN_PIN_5, HIGH);
+      delay(600);  // 1秒間ファンをを回す
+      digitalWrite(FAN_PIN_4, LOW);
+
+      delay(600);  // 1秒間ファンをを回す
+      digitalWrite(FAN_PIN_5, LOW);
+
+      delay(8000);
+
+      //非受信時のテスト用 END-----
+
+
+    } else if (runningMode == 1) { //Interaction
+      //ledTask = 0;
+
+
+    } else { //Off
+      //適度に待つ
+      delay(100);
+    }
+  }
+}
+
+
+//稼働モードを管理するタスク
+void modeControl(void* arg) {
+  while (1) {
+
+    // ボタンが押されたらLEDの色を変化させる
+    if (M5.Btn.wasPressed()) {
+      Serial.print("Btn Pressed");
+      Serial.println(change);
+      switch (change) {
+        case 0:
+          M5.dis.drawpix(0, dispColor(0, brightness, 0));//green
+          runningMode = 0;//Auto
+          break;
+        case 1:
+          M5.dis.drawpix(0, dispColor(0, 0, brightness));//blue
+          runningMode =  1;//Interaction
+          break;
+        case 2:
+          M5.dis.drawpix(0, dispColor(brightness, 0, 0));//red
+          runningMode = 2;//Off
+          break;
+        default:
+          break;
+      }
+      if (change >= 2) {
+        change = 0;
+      } else {
+        change++;
+      }
+    }
+
+
+    M5.update();
+    delay(40);
+  }
+}
+
+
+
 void setup() {
   // シリアルモニタの初期化
   Serial.begin(115200);
+  M5.begin(false, false, true);
+  brightness = 0xFF;
+  M5.dis.drawpix(0, dispColor(0, brightness, 0));//green
+  delay(10);
 
   // Wi-Fiモードを設定
   WiFi.mode(WIFI_STA);
@@ -89,10 +185,9 @@ void setup() {
   digitalWrite(FAN_PIN_4, LOW);
   digitalWrite(FAN_PIN_5, LOW);
 
-  //マイコン起動サインとしてLEDつけておく
-  FastLED.addLeds<WS2812B, PIN_LED, GRB>(leds, NUM_LEDS);
-  leds[0] = CRGB(0, 20, 0);  //CRGB::Blue;
-  FastLED.show();
+  //タスクの設定
+  xTaskCreatePinnedToCore(windControl, "windControlTask0", 4096, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(modeControl, "modeControlTask0", 4096, NULL, 1, NULL, 1);
 }
 
 void loop() {
@@ -104,30 +199,5 @@ void loop() {
     Serial.println("Message send failed");
   }
 
-  //非受信時のテスト用 START-----
-  digitalWrite(FAN_PIN_1, HIGH);
-  delay(600);  // 1秒間ファンをを回す
-
-  digitalWrite(FAN_PIN_2, HIGH);
-  delay(600);  // 1秒間ファンをを回す
-  digitalWrite(FAN_PIN_1, LOW);
-
-  digitalWrite(FAN_PIN_3, HIGH);
-  delay(600);  // 1秒間ファンをを回す
-  digitalWrite(FAN_PIN_2, LOW);
-
-  digitalWrite(FAN_PIN_4, HIGH);
-  delay(600);  // 1秒間ファンをを回す
-  digitalWrite(FAN_PIN_3, LOW);
-
-  digitalWrite(FAN_PIN_5, HIGH);
-  delay(600);  // 1秒間ファンをを回す
-  digitalWrite(FAN_PIN_4, LOW);
-
-  delay(600);  // 1秒間ファンをを回す
-  digitalWrite(FAN_PIN_5, LOW);
-
-  delay(300);
-
-  //非受信時のテスト用 END-----
+  delay(1000);
 }
